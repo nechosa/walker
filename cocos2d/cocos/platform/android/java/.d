b47a1aadd3a45358c848699d14660776159356db
Nechoysa Ilya <nechosa@mail.ru>
2017-02-08 21:14:01 +0300

    add cocos2d

---
+++ cocos2d/cocos/platform/android/java/.d
@@ -0,0 +1,4 @@
+/home/ilya/Project/Joystick/cocos2d/cocos/platform/android/java/gen/com/enhance/gameservice/IGameTuningService.java : \
+  /home/ilya/Project/Joystick/cocos2d/cocos/platform/android/java/src/com/enhance/gameservice/IGameTuningService.aidl
+
+/home/ilya/Project/Joystick/cocos2d/cocos/platform/android/java/src/com/enhance/gameservice/IGameTuningService.aidl :
---
+++ cocos2d/cocos/platform/android/java/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/ilya/Project/Joystick/cocos2d/cocos/platform/android/java/bin/AndroidManifest.xml : \
+/home/ilya/Project/Joystick/cocos2d/cocos/platform/android/java/AndroidManifest.xml \
